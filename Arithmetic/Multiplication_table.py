4f1a0091408353e617307fb05938f7e44801f0f6
SK7here <kailash.s1998@gmail.com>
2019-07-07 08:35:35 +0530

    Removing duplicate folders

--- Arithmetic/Multiplication_table.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Show the multiplication table of : "))  
-# using for loop to iterate multiplication 10 times
-    #Note: Range will not consider the end point mentioned
-for i in range(1,11):  
-   print(num,'x',i,'=',num*i) 
